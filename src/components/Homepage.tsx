b62b29e077aa898321bae24d30dc0552173f41df
julienbrg <julien@strat.cc>
2022-09-05 23:03:35 +0200

    fix login

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import {useState} from "react"; 
+import { useEffect, useState } from "react";
 import { Web3Auth } from "@web3auth/web3auth";
-import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { ethers } from "ethers";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import RPC from "./web3RPC";
+// import { ethers } from "ethers";
+import "./App.css";
 
 import {
   PlasmicHomepage,
@@ -14,60 +16,181 @@ export interface HomepageProps extends DefaultHomepageProps {}
 
 function Homepage_(props: HomepageProps, ref: HTMLElementRefOf<"div">) {
 
-  const web3auth = new Web3Auth({
-    
-    clientId: "BMzPnjsIB60JN4nYjVxSVwfdYNZTrS6lKY4JzYJA8y4mllRPMlKdRITPNKpqgV3n9qTgpv6Sa0CXU5HE0GcR7DY",
-    // clientId: process.env.WEB3_AUTH_CLIENT_ID,
-    chainConfig: {
-      chainNamespace: CHAIN_NAMESPACES.EIP155,
-      chainId: "0x5", 
-      rpcTarget: "https://eth-goerli.gateway.pokt.network/v1/lb/6306fd3fea87cf003a87b590",
-      displayName: "Goerli Testnet",
-      blockExplorer: "https://goerli.etherscan.io", // rinkeby, goerli
-      ticker: "ETH",
-      tickerName: "Ethereum",
-    },
-  });
+const clientId = "BMzPnjsIB60JN4nYjVxSVwfdYNZTrS6lKY4JzYJA8y4mllRPMlKdRITPNKpqgV3n9qTgpv6Sa0CXU5HE0GcR7DY";
 
-  const [addr, setAddr] = useState("Please connect.")
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
+  const [addr, setAddr] = useState("Please connect");
 
-  async function connect() {
+  useEffect(() => {
+    const init = async () => {
+      try {
 
+      const web3auth = new Web3Auth({
+        clientId,
+        chainConfig: {
+          chainNamespace: CHAIN_NAMESPACES.EIP155,
+          chainId: "0x5",
+          rpcTarget: "https://eth-goerli.gateway.pokt.network/v1/lb/6306fd3fea87cf003a87b590", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+        },
+      });
+
+      setWeb3auth(web3auth);
+
+      await web3auth.initModal();
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        };
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const toggle = async () => {
     console.log("clicked")
-
-    await web3auth.initModal();
-
-    const web3authProvider:any = web3auth.connect();
-
-    const provider = new ethers.providers.Web3Provider(web3authProvider); // web3auth.provider
-
-    console.log("provider: ", provider)
-
-    setAddr("0xFrancis")
-
+    if (provider) {
+      await logout();
+    } else {
+      await login();
+    }
   }
 
-  function click() {
+  const login = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    await getAccounts()
+  };
 
-    console.log("clicked")
-    setAddr("default")
+  // const getUserInfo = async () => {
+  //   if (!web3auth) {
+  //     console.log("web3auth not initialized yet");
+  //     return;
+  //   }
+  //   const user = await web3auth.getUserInfo();
+  //   console.log(user);
+  // };
 
-  }
+  const logout = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
 
+  // const getChainId = async () => {
+  //   if (!provider) {
+  //     console.log("provider not initialized yet");
+  //     return;
+  //   }
+  //   const rpc = new RPC(provider);
+  //   const chainId = await rpc.getChainId();
+  //   console.log(chainId);
+  // };
+  const getAccounts = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    console.log(address);
+    setAddr(address);
+  };
+
+  // const getBalance = async () => {
+  //   if (!provider) {
+  //     console.log("provider not initialized yet");
+  //     return;
+  //   }
+  //   const rpc = new RPC(provider);
+  //   const balance = await rpc.getBalance();
+  //   console.log(balance);
+  // };
+
+  // const sendTransaction = async () => {
+  //   if (!provider) {
+  //     console.log("provider not initialized yet");
+  //     return;
+  //   }
+  //   const rpc = new RPC(provider);
+  //   const receipt = await rpc.sendTransaction();
+  //   console.log(receipt);
+  // };
+
+  // const signMessage = async () => {
+  //   if (!provider) {
+  //     console.log("provider not initialized yet");
+  //     return;
+  //   }
+  //   const rpc = new RPC(provider);
+  //   const signedMessage = await rpc.signMessage();
+  //   console.log(signedMessage);
+  // };
+
+  // const getPrivateKey = async () => {
+  //   if (!provider) {
+  //     console.log("provider not initialized yet");
+  //     return;
+  //   }
+  //   const rpc = new RPC(provider);
+  //   const privateKey = await rpc.getPrivateKey();
+  //   console.log(privateKey);
+  // };
+  // const loggedInView = (
+  //   <>
+  //      <button onClick={getUserInfo} className="card">
+  //       Get User Info
+  //     </button>
+  //     <button onClick={getChainId} className="card">
+  //       Get Chain ID
+  //     </button>
+  //     <button onClick={getAccounts} className="card">
+  //       Get Accounts
+  //     </button>
+  //     <button onClick={getBalance} className="card">
+  //       Get Balance
+  //     </button>
+  //     <button onClick={sendTransaction} className="card">
+  //       Send Transaction
+  //     </button>
+  //     <button onClick={signMessage} className="card">
+  //       Sign Message
+  //     </button>
+  //     <button onClick={getPrivateKey} className="card">
+  //       Get Private Key
+  //     </button>
+  //     <button onClick={logout} className="card">
+  //       Log Out
+  //     </button> 
+
+  //     <div id="console" style={{ whiteSpace: "pre-line" }}>
+  //       <p style={{ whiteSpace: "pre-line" }}></p>
+  //     </div>
+  //   </>
+  // );
+
+  // const unloggedInView = (
+  //   <button onClick={login} className="card">
+  //     Login
+  //   </button>
+  // );
 
   return <PlasmicHomepage root={{ ref }} {...props} 
   
-  action={{
-    props: {
-      onClick: () => click()
-     }
-  }}
-
   connect={{
     props: {
-      children: "Connect",
-      onClick: () => connect()
-     }
+      children:(!provider ? "Login" : "Logout"),
+      onClick: () => toggle()
+    } as any
   }}
 
   address={{
